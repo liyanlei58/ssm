0bab1fb65e3c4ff76d8001d16766e3df1339dbc0
yangzhijie <809318068@qq.com>
2021-03-29 23:37:37 +0800

    1.修改数据库名称 2.添加初始化SQL 3.新增ReadMe.md文件，说明测试方法。

---
+++ src/main/resources/init.sql
@@ -0,0 +1,20 @@
+
+CREATE DATABASE db1 CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE db2 CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE db1.`t_student` (
+                             `id` INT ( 11 ) NOT NULL AUTO_INCREMENT,
+                             `name` VARCHAR ( 255 ) DEFAULT NULL,
+                             `age` INT ( 11 ) DEFAULT NULL,
+                             `classname` VARCHAR ( 255 ) DEFAULT NULL,
+                             PRIMARY KEY ( `id` )
+) ENGINE = INNODB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE db2.`t_teacher` (
+                             `id` INT ( 11 ) NOT NULL AUTO_INCREMENT,
+                             `name` VARCHAR ( 255 ) DEFAULT NULL,
+                             `age` INT ( 11 ) DEFAULT NULL,
+                             `subject` VARCHAR ( 255 ) DEFAULT NULL,
+                             PRIMARY KEY ( `id` )
+) ENGINE = INNODB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
